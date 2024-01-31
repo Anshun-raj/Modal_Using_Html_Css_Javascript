c19d63c277ec0d2bfb76a2f57b9139c92a34b67c
Anshun-raj <anshur61500@gmail.com>
2024-01-31 13:07:32 +0530

    First Commit

---
+++ Script.js
@@ -0,0 +1,17 @@
+function ShowSignIn() {
+  document.getElementById("signUp").style.visibility = "visible";
+  document.getElementById("login").style.visibility = "hidden";
+}
+
+function ShowLogin() {
+  document.getElementById("login").style.visibility = "visible";
+  document.getElementById("signUp").style.visibility = "hidden";
+}
+
+function hideData1() {
+  document.getElementById("login").style.visibility = "hidden";
+}
+
+function hideData2() {
+  document.getElementById("signUp").style.visibility = "hidden";
+}
